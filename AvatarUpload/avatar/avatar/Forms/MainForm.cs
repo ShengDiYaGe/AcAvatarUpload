b184c60cbd87c154ff9b1708b78df645757a4453
于志强 <1963260657@qq.com>
2019-04-26 13:43:46 +0800

    format code

--- AvatarUpload/avatar/avatar/Forms/MainForm.cs
+++ AvatarUpload/avatar/avatar/Forms/MainForm.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.ComponentModel;
 using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
-using Newtonsoft.Json.Linq;
 
 namespace avatar
 {
